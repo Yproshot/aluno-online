837c9fa2f0b65d90baa3bc4a5d680d95b4c6475d
Yproshot <maiconpacheco@live.com>
2023-06-28 11:49:40 -0300

    media final

--- src/models/nota .test.js
+++ src/models/nota .test.js
@@ -1,34 +1,36 @@
-function soma(a, b){
-	return a+b
+class Nota {
+    constructor(disciplina, a1, a2, a3) {
+        this.disciplina = disciplina;
+        this.a1 = a1;
+        this.a2 = a2;
+        this.a3 = a3;
+    }
+
+    mediaFinal() {
+        return Math.max(
+            0.4 * this.a1 + 0.6 * this.a2, 
+            0.4 * this.a1 + 0.6 * this.a3, 
+            0.4 * this.a3 + 0.6 * this.a2
+        );
+    }
+
+    mediaCA() {
+        const media = this.mediaFinal();
+
+        if (media >= 0 && media < 2.5) {
+            return "II";
+        } else if (media >= 2.5 && media < 5) {
+            return "MI";
+        } else if (media >= 5 && media < 7.5) {
+            return "MM";
+        } else if (media >= 7.5 && media < 9) {
+            return "MS";
+        } else if (media >= 9 && media <= 10) {
+            return "SS";
+        } else {
+            throw new Error("Nota inválida.");
+        }
+    }
 }
-module.exports = soma
 
-
-
-const soma = require('./soma')
-
-function media(a, b){
-    s = soma(a, b)
-    m = s/2
-    return m
-}
-exports.media = media
-
-
-
-
-const calculo = require('./media')
-
-test('Testando a media aritmética', () => {    
-      expect(calculo.media(7, 4)).toBe(5)
-})
-
-
-/*3) Criar testes unitários utilizando Jest e implementar a função mediaCA() que deve retornar o conceito final com base na escala abaixo: [5,0 pontos]
-9,0 – 10  | SS – Superior
-7,0 – 8,9 | MS – Médio Superior
-5,0 – 6,9 | MM – Médio
-3,0 – 4,9 | MI – Médio Inferior
-0,1 – 2,9 | II – Inferior
-0,0       | SR – Sem rendimento
-*/
+module.exports = Nota;
--- src/models/nota.js
+++ src/models/nota.js
@@ -1,52 +1,36 @@
 class Nota {
-    constructor(disciplina, a1, a2, a3) {
-        this.disciplina = disciplina;
-        this.a1 = a1;
-        this.a2 = a2;
-        this.a3 = a3;
-    }
+  constructor(disciplina, a1, a2, a3) {
+      this.disciplina = disciplina;
+      this.a1 = a1;
+      this.a2 = a2;
+      this.a3 = a3;
+  }
 
-    mediaFinal() {
-        return Math.max(
-            0.4 * this.a1 + 0.6 * this.a2, 
-            0.4 * this.a1 + 0.6 * this.a3, 
-            0.4 * this.a3 + 0.6 * this.a2
-        );
-    }
+  mediaFinal() {
+      return Math.max(
+          0.4 * this.a1 + 0.6 * this.a2, 
+          0.4 * this.a1 + 0.6 * this.a3, 
+          0.4 * this.a3 + 0.6 * this.a2
+      );
+  }
 
-    
+  mediaCA() {
+      const media = this.mediaFinal();
 
-        mediaCA() {
-           
-
-           var media = (a1 + a2 + a3) / 3;
-
-            if (media >= 9.0 && media <= 10.0) {
-              return "SS - Superior";
-            } else if (media >= 7.0 && media < 9.0) {
-              return "MS - Médio Superior";
-            } else if (media >= 5.0 && media < 7.0) {
-              return "MM - Médio";
-            } else if (media >= 3.0 && media < 5.0) {
-              return "MI - Médio Inferior";
-            } else if (media >= 0.1 && media < 3.0) {
-              return "II - Inferior";
-            } else if (media === 0.0) {
-              return "SR - Sem rendimento";
-            } else {
-              return "Nota inválida";
-            }
-            console.log(media);
-          }
-        }
+      if (media >= 0 && media < 2.5) {
+          return "II";
+      } else if (media >= 2.5 && media < 5) {
+          return "MI";
+      } else if (media >= 5 && media < 7.5) {
+          return "MM";
+      } else if (media >= 7.5 && media < 9) {
+          return "MS";
+      } else if (media >= 9 && media <= 10) {
+          return "SS";
+      } else {
+          throw new Error("Nota inválida.");
+      }
+  }
+}
 
 module.exports = Nota;
-
-/*3) Criar testes unitários utilizando Jest e implementar a função mediaCA() que deve retornar o conceito final com base na escala abaixo: [5,0 pontos]
-9,0 – 10  | SS – Superior
-7,0 – 8,9 | MS – Médio Superior
-5,0 – 6,9 | MM – Médio
-3,0 – 4,9 | MI – Médio Inferior
-0,1 – 2,9 | II – Inferior
-0,0       | SR – Sem rendimento
-*/
